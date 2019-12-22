7d866d3cbde88ecc9eb11de87694849372a896c0
awb99 <andreas.wolfgang.bauer@gmail.com>
2019-12-22 17:57:25 -0500

    pinkie added

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject  org.pinkgorilla/kernel-cljs-shadow "0.0.17"
+(defproject  org.pinkgorilla/kernel-cljs-shadow "0.0.18"
   :description "A cljs kernel using shadow-cljs for PinkGorilla Notebook."
   :url "https://github.com/pink-gorilla/kernel-cljs-shadow"
   :license {:name "MIT"}
@@ -7,8 +7,10 @@
                              :username "pinkgorillawb"
                              :sign-releases false}]]
   :dependencies
-  [ ; dependencies of cljs-kernel-shadow. The gorilla-notebook will automatically fetch this transient dependencies
+  [; dependencies of cljs-kernel-shadow. The gorilla-notebook will automatically fetch this transient dependencies
    [thheller/shadow-cljs "2.8.80"] ; needed for the deopendency loader in self hosted clojurescript
    [cljs-await "1.0.2"]  ; "promisify callbacks"
+   [reagent "0.8.1"
+    :exclusions [org.clojure/tools.reader]] ; add custom renderers to pinkie.
    ]
   :source-paths ["src"])
---
+++ src/pinkgorilla/kernel/pinkie.cljs
@@ -0,0 +1,16 @@
+(ns pinkgorilla.kernel.pinkie 
+  (:require 
+   [goog.object :as gobj] 
+   [reagent.core :as r :refer [atom]] 
+   [reagent.impl.template] ))
+
+
+(def custom-renderers (atom {}))
+
+
+(defn register-tag [k v] 
+  (swap! custom-renderers assoc k v)
+  (gobj/set reagent.impl.template/tag-name-cache (name k) v))
+
+
+
