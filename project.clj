1382430eb8a049771c49c681876abefa02d45263
CI/CD <ci@pinkgorilla.org>
2020-03-09 14:27:09 +0000

    Release 0.0.21

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject  org.pinkgorilla/kernel-cljs-shadow "0.0.21-SNAPSHOT"
+(defproject  org.pinkgorilla/kernel-cljs-shadow "0.0.21"
   :description "A cljs kernel using shadow-cljs for PinkGorilla Notebook."
   :url "https://github.com/pink-gorilla/kernel-cljs-shadow"
   :license {:name "MIT"}
