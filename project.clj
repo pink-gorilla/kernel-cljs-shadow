271f86d77fbd016713523201cafff7e54bc288d4
awb99 <andreas.wolfgang.bauer@gmail.com>
2020-03-26 18:06:52 -0500

    nothing

--- project.clj
+++ project.clj
@@ -7,7 +7,7 @@
                                      :password :env/release_password
                                      :sign-releases false}]]  
   :dependencies  [; dependencies of cljs-kernel-shadow. The gorilla-notebook will automatically fetch this transient dependencies
-                  [thheller/shadow-cljs "2.8.80"] ; needed for the deopendency loader in self hosted clojurescript
+                  [thheller/shadow-cljs "2.8.80"] ; needed for the dependency loader in self hosted clojurescript
                   [cljs-await "1.0.2"]  ; "promisify callbacks"
                   [reagent "0.8.1"
                    :exclusions [org.clojure/tools.reader]] ; add custom renderers to pinkie.
